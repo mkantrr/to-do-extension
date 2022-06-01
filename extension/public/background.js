04748630d86f3e59a7befad243d79b5c14c286bd
Matthew Kanter <mattkanter1@outlook.com>
2022-06-01 11:51:03 -0400

    started to implement chrome storage to store state of extension

--- extension/public/background.js
+++ extension/public/background.js
@@ -1,21 +1,7 @@
-chrome.runtime.onSuspend.addListener((handleSaveState()) => {
-    this.state = {
-        listItems: chrome.storage.sync.get('listItems', (result) => {
-            setState({ listItems: result });
-        }),
-        textEntered: chrome.storage.sync.get('textEntered', (result) => {
-            setState({ textEntered: result });
-        }),
-        toggleDelete: chrome.storage.sync.get('toggleDelete', (result) => {
-            setState({ toggleDelete: result });
-        })
-    }
-});
+let defaultState = null;
 
-chrome.runtime.onStartup.addListener((handleSaveState()) => {
-    chrome.storage.sync.set({
-        listItems: [],
-        textEntered: '',
-        toggleDelete: false
+chrome.runtime.onStartup.addListener( () => {
+    chrome.storage.sync.get({ "state": defaultState }, (data) => {
+        chrome.storage.sync.set({ "state": data.state }, function() {});
     });
 });
